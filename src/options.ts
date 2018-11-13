31fef20cc15f90af9d5ec10893a442c6b9a6d57d
Derek Honerlaw <derek@Dereks-MBP-3.lan>
2018-11-12 21:39:04 -0500

    remove unused all options

--- src/options.ts
+++ src/options.ts
@@ -30,31 +30,3 @@ export interface IPluginOptions {
     vpc: IVPCOptions;
     services: IServiceOptions[];
 }
-
-
-export interface IAllOptions {
-    executionRoleArn?: string; // execution role for services, generated if not specified
-    vpc: {
-        cidr: string;
-        subnets: string[]; // subnet cidrs
-    };
-    services: Array<{
-        name: string; // name of the service
-        cpu: number;
-        memory: number;
-        port: number; // docker port (the port exposed on the docker image)
-        entryPoint: string[]; // same as docker's entry point
-        protocols: Array<{
-            protocol: "HTTP" | "HTTPS";
-            certificateArns?: string[]; // needed for https
-        }>;
-        imageRepository: string;
-        imageTag?: string; //
-        priority?: number; // priority for routing, defaults to 1
-        path?: string; // path the Load Balancer should send traffic to, defaults to '*'
-        desiredCount?: number; // defaults to 1
-        taskRoleArn?: string;
-        healthCheckUri?: string; // defaults to "/"
-        healthCheckProtocol?: string; // defaults to "HTTP"
-    }>
-}

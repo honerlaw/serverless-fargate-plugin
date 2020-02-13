2bd0d1b910b2e4fce73545eba9469a3c185c803a
Gabriel Debes <gabriel.wady@gmail.com>
2020-02-12 17:44:00 -0800

    Sanity check for cluster config

--- src/index.ts
+++ src/index.ts
@@ -25,17 +25,19 @@ class ServerlessFargatePlugin {
 
         //For each cluster
         for (let clusterOption of options) {
-            const index = options.indexOf(clusterOption);
-            //multiple self-created VPCs will be a problem here, TODO: solve this with cluster prefix on resouces
-            const vpc: VPC = new VPC(stage, clusterOption.vpc);
-            const cluster: Cluster = new Cluster(stage, clusterOption, vpc);
+            if (clusterOption && clusterOption.vpc) { //sanity check for empty objects
+                const index = options.indexOf(clusterOption);
+                //multiple self-created VPCs will be a problem here, TODO: solve this with cluster prefix on resouces
+                const vpc: VPC = new VPC(stage, clusterOption.vpc);
+                const cluster: Cluster = new Cluster(stage, clusterOption, vpc);
 
-            // merge all our stuff into resources
-            Object.assign(
-                this.serverless.service.provider.compiledCloudFormationTemplate.Resources,
-                vpc.generate(),
-                cluster.generate()
-            );
+                // merge all our stuff into resources
+                Object.assign(
+                    this.serverless.service.provider.compiledCloudFormationTemplate.Resources,
+                    vpc.generate(),
+                    cluster.generate()
+                );
+            }
         }
     }
 
